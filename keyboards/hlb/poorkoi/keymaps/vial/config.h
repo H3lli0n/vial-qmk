dbf3ce32f36defd7d0cfdd03fdc185ab02603d35
H3lli0n <thulium.hqfr@gmail.com>
2024-08-17 21:27:29 +0200

    feat: initial PoorKoi keymap

---
+++ keyboards/hlb/poorkoi/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE8, 0x87, 0xFD, 0x48, 0x56, 0xCB, 0xA9, 0x09}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
