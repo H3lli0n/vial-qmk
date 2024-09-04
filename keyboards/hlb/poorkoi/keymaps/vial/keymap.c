b6c25d2a1709ad08aee013bab20adf842c829b88
H3lli0n <thulium.hqfr@gmail.com>
2024-09-04 22:53:41 +0200

    fix: wrong column

--- keyboards/hlb/poorkoi/keymaps/vial/keymap.c
+++ keyboards/hlb/poorkoi/keymaps/vial/keymap.c
@@ -23,15 +23,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,       KC_Y,      KC_U,      KC_I,      KC_O,       KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,       KC_H,      KC_J,      KC_K,      KC_L,       KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
   KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,      KC_N,      KC_M,      KC_COMM,    KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-  KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,               KC_SPC,               KC_SPC,                KC_RALT, KC_RGUI, KC_APP,  KC_RCTL),
+  KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,               KC_SPC,               KC_SPC,                KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL),
 
 // basic function layer
 [1] = LAYOUT_all(
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
   KC_TRNS,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,   KC_TRNS,   KC_HOME,   KC_UP,     KC_END,      KC_PGUP,  KC_VOLU,  KC_TRNS, KC_TRNS,
   KC_TRNS,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_TRNS,   KC_TRNS,   KC_LEFT,   KC_DOWN,   KC_RIGHT,    KC_PGDN,  KC_VOLD,  KC_TRNS, KC_TRNS,
-  KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_PSCR,  KC_MUTE,  KC_TRNS, KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,              KC_TRNS,              KC_TRNS,               MO(2),    KC_TRNS,  KC_TRNS, KC_TRNS),
+  KC_TRNS,  KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_PSCR,  KC_MUTE,  KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,              KC_TRNS,              KC_TRNS,               KC_TRNS,  MO(2),    KC_TRNS, KC_TRNS),
 
 
 // rgb/media layer
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_VOLU,   KC_VOLD,   KC_MUTE,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
   RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_SPI,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
   KC_TRNS,  RGB_RMOD, RGB_HUD,  RGB_SAD,  RGB_SPD,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-  KC_TRNS,  RGB_VAI,  RGB_VAD,  KC_TRNS,  KC_TRNS,   KC_TRNS,   QK_BOOT,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_TRNS,  RGB_VAI,  RGB_VAD,  KC_TRNS,   KC_TRNS,   QK_BOOT,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,              KC_TRNS,              KC_TRNS,               KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS)
 };
 
