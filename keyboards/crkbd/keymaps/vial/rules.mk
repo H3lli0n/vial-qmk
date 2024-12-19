a4a0e1549230f7e50fa89a7afcc1b1c5b5079cc0
H3lli0n <thulium.hqfr@gmail.com>
2024-12-19 14:13:02 +0100

    feat: custom chocofi rp2040 oled

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,13 +1,9 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
-LTO_ENABLE          = yes
 
 RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
-MOUSEKEY_ENABLE     = no
-OLED_ENABLE         = no
-OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = no
-COMBO_ENABLE        = no
+WPM_ENABLE 			= yes
+OLED_ENABLE         = yes
 
-QMK_SETTINGS        = no
+BOOTLOADER 			= rp2040
