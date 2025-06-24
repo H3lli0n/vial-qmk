5ca293ddc52542b2361a66211282e3d18c0c4d69
H3lli0n <thulium.hqfr@gmail.com>
2025-06-24 22:11:54 +0200

    feat: rgn enabled on D60 Lite

--- keyboards/dztech/dz60rgb_wkl/keymaps/vial/rules.mk
+++ keyboards/dztech/dz60rgb_wkl/keymaps/vial/rules.mk
@@ -1,7 +1,14 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
 # Disable features for space
 COMBO_ENABLE = no # No combos
 KEY_OVERRIDE_ENABLE = no # No key overrides
-RGB_MATRIX_ENABLE = no # No RGB
+RGB_MATRIX_ENABLE = yes # No RGB
 LTO_ENABLE = yes # Link-time optimization
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
