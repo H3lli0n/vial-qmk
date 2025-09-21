4a4eb4d91f96f81f1f3681f6a46e2958c6faac3e
H3lli0n <thulium.hqfr@gmail.com>
2025-09-21 13:57:23 +0200

    feat: support HLB 60 hotswap and solder pcb

---
+++ keyboards/hlb/hlb60/hlb60_s/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x63, 0x1C, 0x77, 0xCB, 0x2B, 0x49, 0xED, 0x85}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 3 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define VIAL_TAP_DANCE_ENTRIES 10
+#define VIAL_COMBO_ENTRIES 15
+#define VIAL_KEY_OVERRIDE_ENTRIES 10
